8ad6c345c0bac119f221cb583c77d2351313f5ab
MANJEET-DevOps <MANJEET.052004@GMAIL.COM>
2025-01-08 22:02:28 +0530

    Java's Variable Declaration (4th-part2)

---
+++ Chardemo.java
@@ -0,0 +1,10 @@
+// Write some programs showing Java's Variable Declaration and use of literals. 
+
+public class Chardemo {
+    public static void main(String[] args) {
+        char ch1,ch2;
+        ch1=88;
+        ch2='Y';
+        System.out.println(ch1+" "+ch2);
+    }
+}
