6179bedffe41ea42df7d7f1b76cca6862140b44a
MANJEET-DevOps <MANJEET.052004@GMAIL.COM>
2025-01-08 22:19:42 +0530

    Simple If_Else (Single statement )-(7th - part 1)

---
+++ Simple_if.java
@@ -0,0 +1,11 @@
+// Simple IF ELSE (Single Statement)
+
+public class Simple_if {
+    public static void main(String[] args) {
+        int x=5,y=7;
+        if(x<y)
+        System.out.println("y is max.");
+        else
+         System.out.println("x is max.");
+    }
+}
