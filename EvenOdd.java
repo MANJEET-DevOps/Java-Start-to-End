75175b2708073736b713380c7e569ca2ff1a448e
MANJEET-DevOps <MANJEET.052004@GMAIL.COM>
2025-01-08 22:22:00 +0530

    If_Else Blocks (Multiple Statement in a if block)-(7th part 2)

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+// IF ELSE BLOCKS (Multiple Statements in a if block)
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int x=5,y=8;
+        if(y%2==0){
+         System.out.println("y is even.");
+         x=y;
+        }
+        else{
+            System.out.println("y is odd");
+            x=y;
+        }
+    }
+}
