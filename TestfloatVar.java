2b134b32869ba99dc297ba13f5d29474ad7d1a81
MANJEET-DevOps <MANJEET.052004@GMAIL.COM>
2025-01-08 22:04:00 +0530

    Java's Variable Declaration (4th-part 3)

---
+++ TestfloatVar.java
@@ -0,0 +1,12 @@
+// Write some programs showing Java's Variable Declaration and use of literals. 
+
+
+public class TestfloatVar {
+    public static void main(String[] args) {
+        float pi=3.14f;
+        double area;
+        float radius = 9;
+        area = pi*radius*radius;
+        System.out.println("Area of the Circle = " +area);
+    }
+}
