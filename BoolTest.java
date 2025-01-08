6d7107e579cbd52080d297c00b65ea58f7ccf80e
MANJEET-DevOps <MANJEET.052004@GMAIL.COM>
2025-01-08 22:04:51 +0530

    Java's Variable Declaration(4th-part 4)

---
+++ BoolTest.java
@@ -0,0 +1,11 @@
+public class BoolTest {
+    public static void main(String[] args) {
+        boolean b;
+        b=false;
+        if(b){   // same as if(b=true)
+            System.out.println("b is True");
+        }else{
+            System.out.println("b is False");
+        }
+    }
+}
