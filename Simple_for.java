9ff8b59ef96863e68973679e5e057e45bf721b4b
MANJEET-DevOps <MANJEET.052004@GMAIL.COM>
2025-01-16 14:08:50 +0530

    Loop demo(10th part 1)

---
+++ Simple_for.java
@@ -0,0 +1,10 @@
+// A simple for loop demo
+ 
+public class Simple_for {
+    public static void main(String[] args) {
+        int i;
+        // Print the table of 2
+        for(i=1; i<=20; i=2*i)
+        System.out.println(i);
+    }
+}
